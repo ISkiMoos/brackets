da4b79a2d22abc917f23d40f249c5ee634c299fc
ISkiMoos <alexandrglushkoff@gmail.com>
2019-03-24 10:20:33 +0300

    tree/feature: add function check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let bracketArr = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      let b = {
+        openBracket: bracketsConfig[i][0],
+        closeBracket: bracketsConfig[i][1]
+      }
+      bracketArr.push(b);
+  }
+
+  for (let i = 0; i < 151; i++){
+      for(let j = 0; j < bracketArr.length; j++) {
+        str = str.replace(bracketArr[j].openBracket + '' + bracketArr[j].closeBracket, '');
+      }
+  }
+
+  if(str.length == 0) {
+    return true;
+  } else {
+      return false;
+    }
+
 }
